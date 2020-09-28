5b66c68243dbd0bc734652cab8cced377f2fbf13
Piyush Singh <bpiyushsingh@gmail.com>
2020-09-28 19:25:34 +0530

    task: add review and rating feature

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -14,6 +14,7 @@ const Bootcamp = require('../models/Bootcamp');
 
 // Include other resource routers
 const courseRouter = require('./courses');
+const reviewsRouter = require('./reviews');
 
 const router = express.Router();
 
@@ -22,6 +23,7 @@ const { protect, authorize } = require('../middleware/auth');
 
 // Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
+router.use('/:bootcampId/reviews', reviewsRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
--- server.js
+++ server.js
@@ -15,9 +15,10 @@ dotenv.config({ path: './config/config.env' });
 connectDB();
 
 //Load Routes
+const auth = require('./routes/auth');
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
-const auth = require('./routes/auth');
+const reviews = require('./routes/reviews');
 const users = require('./routes/users');
 
 const app = express();
@@ -42,9 +43,10 @@ app.use(fileupload());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Mount Routers
+app.use('/api/v1/auth', auth);
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
-app.use('/api/v1/auth', auth);
+app.use('/api/v1/reviews', reviews);
 app.use('/api/v1/users', users);
 
 app.use(errorHandler);
